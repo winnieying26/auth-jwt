7c4d4f2468c3b85e52fbecd4e142d6bfae7aa92f
winnieying26 <winniequan55@gmail.com>
2025-01-16 00:11:56 -0500

    authroutees auth.controllers

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { connectDB } from "./db/connectDB.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js"
+
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000
+app.use(express.json()); // allow us to parse incoming requests with JSON payload: req.body
+app.use(cookieParser())
+;
+app.get("/", (req, res) => {
+  res.send("Hello world 123");
+});
+
+app.use("/api/auth", authRoutes)
+
+app.listen(3000, () => {
+  connectDB();
+  console.log(`Server is running on port ${port}`);
+});
+
+// WdADCeM7HCBvGiZB
---
+++ backend/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { forgotPassword, login, logout, signup, verifyEmail,resetPassword } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+router.post("/verify-email", verifyEmail);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword)
+
+
+export default router;
